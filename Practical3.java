8152df8b5e4d798da1f194a70df274a455bfe05e
Kishan Joshi <kishanjoshi0317@gmail.com>
2026-01-14 20:44:36 +0530

    Update Practical3.java

--- Practical3.java
+++ Practical3.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Practical 3 {
+public class Practical3 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
